af7e8de7f60122a021a532889bcc74bfdd06ca9a
ggrossvi <hello.gloria@gmail.com>
2021-06-03 15:20:22 -0700

    Recursion Homework

--- part-1.py
+++ part-1.py
@@ -4,17 +4,59 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(num):
+    # base case 
+    if num < 0:
+        raise ValueError("num is less than 0")
+    elif num == 0:
+        # print("num is 0")
+        return 1
+        
+    # print(num - 1)
+    #recursive    
+    return factorial(num -1) * num
 
 
 # reverse
 
+def factorial(num):
+    # base case 
+    if num < 0:
+        raise ValueError("num is less than 0")
+    elif num == 0:
+        # print("num is 0")
+        return 1
+        
+    # print(num - 1)
+    #recursive    
+    return factorial(num -1) * num
 
 
 # bunny
 
+def bunny(count):
+    #base case
+    if count == 0: 
+        return 0
+    if count == 1:
+        return 2
+        
+    #recursive  
+    return bunny(count-1) + 2
 
 
 # is_nested_parens
+
+def is_nested_parens(parens):  
+    #base cases - stopping condition
+    if parens == "":
+        return True
+    if parens[0] != "(":
+        return False
+    if parens[-1] != ")":
+        return False
+
+    #recursion
+    return True and is_nested_parens(parens[1:-1])
 
 
--- part-2.py
+++ part-2.py
@@ -5,12 +5,72 @@
 
 # search
 
+def search(array,query):
+    #base case 
+    print(array)
+    if array == []:    
+        return False
+        
+    if array[0] == query:
+        return True
+        
+        
+    return search(array[1:], query)
+        
+""" These are both the same
+assert search([], "a") == False
+assert not seach([], "a")
+"""
+    
+
 
 
 # is_palindrome
+def is_palindrome(text):
+    #base case -- stoping point
+    print(text)
+    if len(text) == 1:
+        return True
+    if len(text) == 0:
+        return True
+    if text[0] != text[-1]:   
+        return False
+        
+    return is_palindrome(text[1:-1])
+        
 
 
 
 # digit_match
+
+def digit_match(param1,param2):
+    #base case
+    print(param1,param2)
+    if param1 == 0 and param2 == 0: 
+        return 1 
+    
+    if param1 == 0 or param2 == 0:
+        return 0
+        
+    last_digit_param1 = param1  % 10  
+    last_digit_param2 = param2 % 10
+    
+    param1 = param1 // 10
+    param2 = param2 // 10
+    if param1 == 0 and param2 == 0: 
+        if last_digit_param1 == last_digit_param2:
+            return 1 
+        else:
+            return 0
+        
+    if last_digit_param1 == last_digit_param2:
+        return 1 + digit_match(param1,param2)
+    return 0 + digit_match(param1,param2)
+   
+        
+        
+    
+    
+    
 
 
